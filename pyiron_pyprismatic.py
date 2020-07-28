40c22aac9094777521fbabd4418a4f2efaed279c
Ning Wang <wangning2nwpu@gmail.com>
2020-07-28 10:37:10 +0200

    update

--- pyiron_pyprismatic.py
+++ pyiron_pyprismatic.py
@@ -2,6 +2,7 @@ from pyiron.base.job.template import TemplateJob
 import pyprismatic as pr
 from pyprismatic.fileio import readMRC
 import matplotlib.pyplot as plt
+import numpy as np
 
 class PrismaticJob(TemplateJob):
     def __init__(self, project, job_name):
@@ -9,21 +10,18 @@ class PrismaticJob(TemplateJob):
         self.input['filenameAtoms'] = None
         self.input['filenameOutput'] = 'output.mrc'
         self.input['algorithm'] = 'multislice' 
-        
+        self._python_only_job = True
     def run_static(self):
-        meta = Metadata(filenameAtoms=self.input['filenameAtoms'],
+        meta = pr.Metadata(filenameAtoms=self.input['filenameAtoms'],
                  filenameOutput=self.input['filenameOutput'])
         meta.algorithm = self.input['algorithm']
         meta.go()
-        print ('OK 1')
         with self.project_hdf5.open("output/generic") as h5out: 
             result = readMRC(self.input['filenameOutput'])
-            print ('OK 2')
             h5out["result"] = result 
-            print ('OK 3')
         self.status.finished = True
 
-    def plotting():
+    def plotting(self):
         result = self["output/generic/result"]
         plt.imshow(np.squeeze(np.sum(result, axis=2)))
 
